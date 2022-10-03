1912dde8d4892fa33e0145ff16b303a2eb942682
IronAdministrator <74884776+IronAdministrator@users.noreply.github.com>
2022-10-03 23:49:01 +0200

    project planner base compelete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
+    <FilterNav @filterChange="current = $event" :current="current" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects.reverse()" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
-        <!-- <p>{{project.title}}</p> -->
       </div>
     </div>
   </div>
@@ -13,14 +13,17 @@
 // @ is an alias to /src
 
 import SingleProject from "../components/SingleProject.vue";
+import FilterNav from "@/components/FilterNav.vue";
 export default {
   name: 'HomeView',
   components: {
     SingleProject,
+    FilterNav
 },
   data() {
     return {
-      projects: []
+      projects: [],
+      current: 'all'
     }
   },
   mounted () {
@@ -28,19 +31,30 @@ export default {
       .then((res) => {return res.json()})
       .then((data) => {this.projects = data})
       .catch((err) => {console.log(err.message)})
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return id !== project.id;
+      })
     },
-    methods: {
-      handleDelete(id) {
-        this.projects = this.projects.filter((project) => {
-          return id !== project.id;
-        })
-      },
-      handleComplete(id) {
-        let p = this.projects.find((project) => {
-          return project.id === id
-        })
-        p.complete = !p.complete
-      }
-    },
+    handleComplete(id) {
+      let p = this.projects.find((project) => {
+        return project.id === id
+      })
+      p.complete = !p.complete
+    }
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter(project => project.complete)
+      } 
+      if (this.current === 'ongoing') {
+        return this.projects.filter(project => !project.complete)
+      } 
+      return [...this.projects]
+    }
+  }
 }
 </script>
